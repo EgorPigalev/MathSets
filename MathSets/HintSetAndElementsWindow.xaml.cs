459fc407550f1d2a3f7eaea9eae1dddbd59bce27
AleksandraLebedeva <sashyla1809@gmail.com>
2023-04-02 22:25:19 +0300

    Добавлено  окно подсказки к урокам 1-2 "Множество и его элементы"

--- MathSets/HintSetAndElementsWindow.xaml.cs
+++ MathSets/HintSetAndElementsWindow.xaml.cs
@@ -10,30 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MathSets.pages
+namespace MathSets
 {
     /// <summary>
-    /// Логика взаимодействия для SetAndElementsSolvingTasksPage.xaml
+    /// Логика взаимодействия для HintSetAndElementsWindow.xaml
     /// </summary>
-    public partial class SetAndElementsSolvingTasksPage : Page
+    public partial class HintSetAndElementsWindow : Window
     {
-        public SetAndElementsSolvingTasksPage()
+        public HintSetAndElementsWindow()
         {
             InitializeComponent();
         }
 
         private void BtnBack_Click(object sender, RoutedEventArgs e)
         {
-            FrameClass.mainFrame.Navigate(new SetAndElementsPage());
-        }
-
-        private void BtnHint_Click(object sender, RoutedEventArgs e)
-        {
-            HintSetAndElementsWindow hint = new HintSetAndElementsWindow();
-            hint.ShowDialog();
+            this.Close();
         }
     }
 }
