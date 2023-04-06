920ccbdcb4bf2794c3b1341d1a956a252829c801
Lena_Borisova <elenabor2003@gmail.com>
2023-04-06 17:07:18 +0300

    Разработка задания на выбор

--- MathSets/pages/CombiningSetsPage.xaml.cs
+++ MathSets/pages/CombiningSetsPage.xaml.cs
@@ -20,9 +20,27 @@ namespace MathSets.pages
     /// </summary>
     public partial class CombiningSetsPage : Page
     {
+        Random random = new Random();
         public CombiningSetsPage()
         {
             InitializeComponent();
+            EllipseOne.StrokeThickness = Base.StrokeThickness;
+            EllipseTwo.StrokeThickness = Base.StrokeThickness;
+            EllipseGeneration ellipseGeneration = new EllipseGeneration();
+            Ellipse ellipseOne = ellipseGeneration.getEllipse(random.Next(50, 200), random.Next(50, 200), random.Next(40, 100), random.Next(0, 50)); // Создание первого эллипса
+            ellipseOne.MouseDown += Ellipse_MouseDown;
+            Cnv.Children.Add(ellipseOne);
+            Ellipse ellipseTwo = ellipseGeneration.getEllipse(random.Next(50, 200), random.Next(50, 200), random.Next(40, 100), random.Next(0, 50)); // Создание второго эллипса
+            ellipseTwo.MouseDown += Ellipse_MouseDown;
+            Cnv.Children.Add(ellipseTwo);
+            Path combinedPath = ellipseGeneration.getUnification(ellipseOne, ellipseTwo, GeometryCombineMode.Intersect); // Создание пересечения
+            combinedPath.MouseDown += Ellipse_MouseDown;
+            Cnv.Children.Add(combinedPath);
+        }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
         }
 
         private void BtnBack_Click(object sender, RoutedEventArgs e)
@@ -31,6 +49,11 @@ namespace MathSets.pages
         }
 
         private void BtnHint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void GenerationInTheCanvas()
         {
 
         }
