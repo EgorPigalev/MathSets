e4383959a3f8357af601e5e809211c4fbf43afe7
AleksandraLebedeva <sashyla1809@gmail.com>
2023-04-13 14:30:10 +0300

    добавила в HintLessonSix закрытье окна, просто из-за него не запускалось приложение

--- MathSets/windows/HintLessonSix.xaml.cs
+++ MathSets/windows/HintLessonSix.xaml.cs
@@ -23,5 +23,10 @@ namespace MathSets.windows
         {
             InitializeComponent();
         }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
